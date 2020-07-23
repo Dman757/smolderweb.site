4f6aead9413cd915f78e216589fa2701a4d483f9
David Meade <dman757@gmail.com>
2020-07-23 00:06:47 -0700

    jank ass filter

--- src/ThemeButton.js
+++ src/ThemeButton.js
@@ -1,15 +1,29 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "./App";
+import "./ThemeButton.css";
+import ha from "./assets/ha.png";
 export default function ThemeButton() {
   const { setTheme } = useContext(ThemeContext);
 
   return (
-    <button
-      onClick={() => {
-        setTheme();
-      }}
-    >
-      Click Me
-    </button>
+    <div className="imageContainer">
+      <div className="filterOverlay" />
+      <input
+        className="themeButton"
+        onClick={() => {
+          setTheme();
+        }}
+        type="image"
+        src={ha}
+      />
+    </div>
+    // <button
+    //   className="themeButton"
+    //   onClick={() => {
+    //     setTheme();
+    //   }}
+    // >
+    //   Click Me
+    // </button>
   );
 }
